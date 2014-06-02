62c3f4adbf6b7e940edb2d5146f457a8e99262f8
Kyle Bailey <kyle.bailey@rutgers.edu>
2014-06-02 18:35:21 -0400

    fixed colors, forgot they had to be in a range of 0.0 to 1.0

--- Rutgers/UIColor+RutgersColors.m
+++ Rutgers/UIColor+RutgersColors.m
@@ -10,9 +10,9 @@
 
 @implementation UIColor (RutgersColors)
 +(UIColor *)rutgersRedColor{
-    return [UIColor colorWithRed:206 green:17 blue:38 alpha:1];
+    return [UIColor colorWithRed:206/255.0 green:17/255.0 blue:38/255.0 alpha:1];
 }
 +(UIColor *)rutgersGreyColor{
-    return [UIColor colorWithRed:102 green:109 blue:112 alpha:1];
+    return [UIColor colorWithRed:102/255.0 green:109/255.0 blue:112/255.0 alpha:1];
 }
 @end
