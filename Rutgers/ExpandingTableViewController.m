99ae16cd60696e2330f5135bee614f93f99cdccc
kyle21b <kyle2194@gmail.com>
2014-06-19 17:42:02 -0400

    Much better expansion code, no jumps, scrolls cell onto view if needed

--- Rutgers/ExpandingTableViewController.m
+++ Rutgers/ExpandingTableViewController.m
@@ -8,6 +8,7 @@
 
 #import "ExpandingTableViewController.h"
 #import "ExpandingTableViewSection.h"
+#import "NSIndexPath+RowExtensions.h"
 
 @interface ExpandingTableViewController ()
 
@@ -15,14 +16,42 @@
 
 @implementation ExpandingTableViewController
 
+-(void)viewDidLoad{
+    [super viewDidLoad];
+    self.tableView.estimatedRowHeight = 0;
+}
+
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     if (indexPath.row != 0) return;
-    ExpandingTableViewSection *section = (ExpandingTableViewSection *)[self sectionInTableView:tableView atIndex:indexPath.section];
     
+    ExpandingTableViewSection *section = self.sections[indexPath.section];
+    
+    NSInteger oldCount = section.numberOfRows;
     section.expanded = !section.expanded;
-    [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:indexPath.section] withRowAnimation:UITableViewRowAnimationFade];
+    NSInteger newCount = section.numberOfRows;
+
+    UITableViewRowAnimation animationType = UITableViewRowAnimationFade;
+    
+    BOOL nowExpanded = section.expanded;
+    if (nowExpanded) {
+        NSArray *insertedIndexPaths = [NSIndexPath indexPathsForRange:NSMakeRange(oldCount, newCount-oldCount) inSection:indexPath.section];
+        [tableView insertRowsAtIndexPaths:insertedIndexPaths withRowAnimation:animationType];
+        NSArray *visibleIndexPaths = [tableView indexPathsForVisibleRows];
+        NSInteger index = [visibleIndexPaths indexOfObject:indexPath];
+        if (index > visibleIndexPaths.count - 4) {
+            [tableView scrollToRowAtIndexPath:[insertedIndexPaths lastObject] atScrollPosition:UITableViewScrollPositionBottom animated:YES];
+        }
+    } else {
+        [tableView deleteRowsAtIndexPaths:[NSIndexPath indexPathsForRange:NSMakeRange(newCount, oldCount-newCount) inSection:indexPath.section] withRowAnimation:animationType];
+    }
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+ //   [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:indexPath.section] withRowAnimation:UITableViewRowAnimationTop];
 }
+
 -(BOOL)tableView:(UITableView *)tableView shouldHighlightRowAtIndexPath:(NSIndexPath *)indexPath{
     return (indexPath.row == 0);
 }
+
 @end
