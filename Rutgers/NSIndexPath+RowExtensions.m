f70e3f0b27675d380bacd530fb75a57b8e43f7de
kyle21b <kyle2194@gmail.com>
2014-06-19 17:42:47 -0400

    category on nsindexpath to get relevant index paths for a range in a section

---
+++ Rutgers/NSIndexPath+RowExtensions.m
@@ -0,0 +1,19 @@
+//
+//  NSIndexPath+RowExtensions.m
+//  Rutgers
+//
+//  Created by Kyle Bailey on 6/16/14.
+//  Copyright (c) 2014 Rutgers. All rights reserved.
+//
+
+#import "NSIndexPath+RowExtensions.h"
+
+@implementation NSIndexPath (RowExtensions)
++(NSArray *)indexPathsForRange:(NSRange)range inSection:(NSInteger)section{
+    NSMutableArray *indexPaths = [NSMutableArray arrayWithCapacity:range.length];
+    for (NSUInteger i = range.location; i < (range.location + range.length); i++) {
+        [indexPaths addObject:[NSIndexPath indexPathForRow:i inSection:section]];
+    }
+    return indexPaths;
+}
+@end
