62b073f2397b73f32c04de6dc05d26ad404e17e1
kyle21b <kyle2194@gmail.com>
2014-06-27 18:24:55 -0400

    Messing around with a custom tab bar that can hide, probably won’t be used

---
+++ Rutgers/RUTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  RUTabBarController.h
+//  Rutgers
+//
+//  Created by Kyle Bailey on 6/26/14.
+//  Copyright (c) 2014 Rutgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RUTabBarController : UITabBarController
+
+@end
