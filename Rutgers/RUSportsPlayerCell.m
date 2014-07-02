8a36bc017d54227a41230ae39df83f0029e8f66f
kyle21b <kyle2194@gmail.com>
2014-07-02 17:53:15 -0400

    increased height of each cell

--- Rutgers/RUSportsPlayerCell.m
+++ Rutgers/RUSportsPlayerCell.m
@@ -18,10 +18,11 @@
     self.playerView = [UIView newAutoLayoutView];
     
     self.nameLabel = [UILabel newAutoLayoutView];
-    self.nameLabel.font = [UIFont systemFontOfSize:16];
+    self.nameLabel.font = [UIFont systemFontOfSize:18];
+    self.nameLabel.adjustsFontSizeToFitWidth = YES;
     
     self.jerseyNumberLabel = [UILabel newAutoLayoutView];
-    self.jerseyNumberLabel.font = [UIFont systemFontOfSize:16];
+    self.jerseyNumberLabel.font = [UIFont systemFontOfSize:18];
     
     self.initialsLabel = [UILabel newAutoLayoutView];
     self.initialsLabel.textAlignment = NSTextAlignmentCenter;
@@ -39,14 +40,14 @@
 }
 
 -(void)initializeConstraints{
-    [self.playerView autoSetDimensionsToSize:CGSizeMake(40, 40)];
+    [self.playerView autoSetDimensionsToSize:CGSizeMake(50, 50)];
     [self.playerView autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:kLabelHorizontalInsets];
     [self.playerView autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:2];
     [self.playerView autoPinEdgeToSuperviewEdge:ALEdgeBottom withInset:2 relation:NSLayoutRelationGreaterThanOrEqual];
     
     [self.initialsLabel autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
-    [self.playerImageView autoSetDimensionsToSize:CGSizeMake(76, 100)];
-    [self.playerImageView autoPinEdge:ALEdgeTop toEdge:ALEdgeTop ofView:self.playerView withOffset:-15];
+    [self.playerImageView autoSetDimensionsToSize:CGSizeMake(95, 125)];
+    [self.playerImageView autoPinEdge:ALEdgeTop toEdge:ALEdgeTop ofView:self.playerView withOffset:-18];
     [self.playerImageView autoAlignAxisToSuperviewAxis:ALAxisVertical];
     
     [self.nameLabel autoPinEdge:ALEdgeLeft toEdge:ALEdgeRight ofView:self.playerView withOffset:10];
@@ -54,6 +55,8 @@
 
     [self.jerseyNumberLabel autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:kLabelHorizontalInsets];
     [self.jerseyNumberLabel autoAlignAxisToSuperviewAxis:ALAxisHorizontal];
+    [self.jerseyNumberLabel setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisHorizontal];
+    [self.jerseyNumberLabel autoPinEdge:ALEdgeLeft toEdge:ALEdgeRight ofView:self.nameLabel withOffset:kLabelHorizontalInsets relation:NSLayoutRelationGreaterThanOrEqual];
 }
 
 -(void)didLayoutSubviews{
