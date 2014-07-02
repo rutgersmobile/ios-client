9dfe7fe8e1e5cac9e02f3a45597b892c726b1693
kyle21b <kyle2194@gmail.com>
2014-07-02 17:46:26 -0400

    renamed file

---
+++ Rutgers/RUTextViewController.h
@@ -0,0 +1,13 @@
+//
+//  text.h
+//  Rutgers
+//
+//  Created by Kyle Bailey on 5/15/14.
+//  Copyright (c) 2014 Rutgers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RUTextViewController : UIViewController
++(instancetype)componentForChannel:(NSDictionary *)channel;
+@end
