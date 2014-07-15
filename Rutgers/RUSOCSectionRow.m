7606988bf003b5041357699aaa809cedc08814b7
kyle21b <kyle2194@gmail.com>
2014-07-15 17:56:54 -0400

    refactoring

--- Rutgers/RUSOCSectionRow.m
+++ Rutgers/RUSOCSectionRow.m
@@ -25,10 +25,7 @@
     self = [super initWithIdentifier:@"RUSOCSectionCell"];
     if (self) {
         self.section = section;
-        self.indexText = [NSString stringWithFormat:@"%@ %@",[self stringForKeypath:@"number"],[self stringForKeypath:@"index"]];
-        self.professorText = [self.section[@"instructors"] firstObject][@"name"];
-        self.descriptionText = [self stringForKeypath:@"sectionNotes"];
-        [self makeOtherTextStrings];
+        [self makeTextStrings];
     }
     return self;
 }
@@ -40,45 +37,41 @@
     cell.dayLabel.text = self.dayText;
     cell.timeLabel.text = self.timeText;
     cell.locationLabel.text = self.locationText;
+    
     if ([self.section[@"openStatus"] boolValue]) {
-        cell.backgroundColor = [UIColor colorWithRed:244/255.0 green:201/255.0 blue:181/255.0 alpha:1];
-    } else {
         cell.backgroundColor = [UIColor colorWithRed:217/255.0 green:242/255.0 blue:213/255.0 alpha:1];
+    } else {
+        cell.backgroundColor = [UIColor colorWithRed:244/255.0 green:201/255.0 blue:181/255.0 alpha:1];
     }
 }
 
--(NSString *)stringForKeypath:(NSString *)keypath{
-    return [self dictionary:self.section stringFromKeypath:keypath];
-}
-
--(NSString *)dictionary:(NSDictionary *)dictionary stringFromKeypath:(NSString *)keypath{
-    NSString *value = [dictionary valueForKeyPath:keypath];
-    return [value isKindOfClass:[NSString class]] ? value : nil;
-}
-
--(void)makeOtherTextStrings{
+-(void)makeTextStrings{
+    self.indexText = [NSString stringWithFormat:@"%@ %@",self.section[@"number"],self.section[@"index"]];
+    self.professorText = [self.section[@"instructors"] firstObject][@"name"];
+    self.descriptionText = self.section[@"sectionNotes"];
+    
     NSMutableString *mutableDayString = [[NSMutableString alloc] init];
     NSMutableString *mutableTimeString = [[NSMutableString alloc] init];
     NSMutableString *mutableLocationString = [[NSMutableString alloc] init];
 
     for (NSDictionary *meetingTime in self.section[@"meetingTimes"]) {
-        NSString *meetingDay = [self dictionary:meetingTime stringFromKeypath:@"meetingDay"];
+        NSString *meetingDay = meetingTime[@"meetingDay"];
         if (meetingDay) {
             [mutableDayString appendString:meetingDay];
             [mutableDayString appendString:@"\n"];
         }
         
-        NSString *startTime = [self dictionary:meetingTime stringFromKeypath:@"startTime"];
-        NSString *endTime = [self dictionary:meetingTime stringFromKeypath:@"endTime"];
+        NSString *startTime = meetingTime[@"startTime"];
+        NSString *endTime = meetingTime[@"endTime"];
         
         if (startTime || endTime) {
             [mutableTimeString appendString:[NSString stringWithFormat:@"%@:%@ - %@:%@",[startTime substringToIndex:2],[startTime substringFromIndex:2],[endTime substringToIndex:2],[endTime substringFromIndex:2]]];
             [mutableTimeString appendString:@"\n"];
         }
         
-        NSString *campusAbbrev = [self dictionary:meetingTime stringFromKeypath:@"campusAbbrev"];
-        NSString *buildingCode = [self dictionary:meetingTime stringFromKeypath:@"buildingCode"];
-        NSString *roomNumber = [self dictionary:meetingTime stringFromKeypath:@"roomNumber"];
+        NSString *campusAbbrev = meetingTime[@"campusAbbrev"];
+        NSString *buildingCode = meetingTime[@"buildingCode"];
+        NSString *roomNumber = meetingTime[@"roomNumber"];
 
         if (meetingDay) {
             [mutableLocationString appendString:[NSString stringWithFormat:@"%@ %@ %@",campusAbbrev,buildingCode,roomNumber]];
@@ -86,9 +79,14 @@
         }
     }
 
-    self.dayText = [mutableDayString copy];
-    self.timeText = [mutableTimeString copy];
-    self.locationText = [mutableLocationString copy];
+    if (mutableDayString.length || mutableTimeString.length || mutableLocationString.length) {
+        self.dayText = [mutableDayString copy];
+        self.timeText = [mutableTimeString copy];
+        self.locationText = [mutableLocationString copy];
+    } else {
+        self.timeText = @"Hours by arr.";
+    }
+
 }
 
 @end
