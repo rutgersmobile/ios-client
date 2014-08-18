6a1325cf91460e6bef861aa4e84f2093da661f37
kyle21b <kyle2194@gmail.com>
2014-08-18 12:06:20 -0400

    navigation controller that allows setting of status bar color

---
+++ Rutgers/RUNavigationController.m
@@ -0,0 +1,25 @@
+//
+//  RUNavigationController.m
+//  Rutgers
+//
+//  Created by Kyle Bailey on 8/15/14.
+//  Copyright (c) 2014 Rutgers. All rights reserved.
+//
+
+#import "RUNavigationController.h"
+
+@implementation RUNavigationController
+
+-(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.preferredStatusBarStyle = UIStatusBarStyleLightContent;
+    }
+    return self;
+}
+
+-(void)setPreferredStatusBarStyle:(UIStatusBarStyle)preferredStatusBarStyle{
+    _preferredStatusBarStyle = preferredStatusBarStyle;
+    [self setNeedsStatusBarAppearanceUpdate];
+}
+@end
