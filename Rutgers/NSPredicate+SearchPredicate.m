430d169a466261ab105ea871d815eaac32c1edd4
kyle21b <kyle2194@gmail.com>
2014-08-18 12:02:34 -0400

    search predicate

---
+++ Rutgers/NSPredicate+SearchPredicate.m
@@ -0,0 +1,31 @@
+//
+//  NSPredicate+SearchPredicate.m
+//  Rutgers
+//
+//  Created by Kyle Bailey on 8/13/14.
+//  Copyright (c) 2014 Rutgers. All rights reserved.
+//
+
+#import "NSPredicate+SearchPredicate.h"
+#import "NSString+WordsInString.h"
+
+@implementation NSPredicate (SearchPredicate)
+
++(NSArray *)subpredicatesForQuery:(NSString *)query keyPath:(NSString *)keyPath{
+    NSArray *wordsInQuery = [query wordsInString];
+    
+    NSMutableArray *predicates = [NSMutableArray array];
+    
+    for (NSString *word in wordsInQuery) {
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:
+                                  @"(%K BEGINSWITH[cd] %@) OR (%K CONTAINS[cd] %@)", keyPath, word, keyPath,[NSString stringWithFormat:@" %@", word]];
+        [predicates addObject:predicate];
+    }
+    
+    return predicates;
+}
+
++(NSPredicate *)predicateForQuery:(NSString *)query keyPath:(NSString *)keyPath{
+    return [NSCompoundPredicate andPredicateWithSubpredicates:[self subpredicatesForQuery:query keyPath:keyPath]];
+}
+@end
