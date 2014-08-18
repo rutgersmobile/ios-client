be09aabfc697c43bec97f09a0979598f8d488665
kyle21b <kyle2194@gmail.com>
2014-08-18 13:35:40 -0400

    added code to ignore loading if not current

--- Rutgers/PlacesSearchDataSource.m
+++ Rutgers/PlacesSearchDataSource.m
@@ -13,14 +13,23 @@
 -(id)init{
     self = [super init];
     if (self) {
-        self.itemLimit = 25;
+        self.itemLimit = 35;
     }
     return self;
 }
 
--(void)updateForSearchString:(NSString *)searchString{
-    [[RUPlacesDataLoadingManager sharedInstance] queryPlacesWithString:searchString completion:^(NSArray *results) {
-        self.items = results;
+-(void)updateForQuery:(NSString *)query{
+    [self loadContentWithBlock:^(AAPLLoading *loading) {
+        [[RUPlacesDataLoadingManager sharedInstance] queryPlacesWithString:query completion:^(NSArray *results) {
+            if (!loading.current) {
+                [loading ignore];
+                return;
+            }
+            
+            [loading updateWithContent:^(typeof(self) me) {
+                me.items = results;
+            }];
+        }];
     }];
 }
 @end
