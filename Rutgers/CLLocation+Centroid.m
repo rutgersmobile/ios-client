ebbb8e09cd6761e1b945927630be165a9db4860a
kyle21b <kyle2194@gmail.com>
2014-08-18 12:01:33 -0400

    category for finding centroid of array of locations

---
+++ Rutgers/CLLocation+Centroid.m
@@ -0,0 +1,22 @@
+//
+//  CLLocation+Centroid.m
+//  Rutgers
+//
+//  Created by Kyle Bailey on 8/13/14.
+//  Copyright (c) 2014 Rutgers. All rights reserved.
+//
+
+#import "CLLocation+Centroid.h"
+
+@implementation CLLocation (Centroid)
++(CLLocation *)centroidOfLocations:(NSArray *)locations{
+    CLLocationDegrees lat = 0;
+    CLLocationDegrees lon = 0;
+    for (CLLocation *location in locations) {
+        CLLocationCoordinate2D coordinate = location.coordinate;
+        lat += coordinate.latitude;
+        lon += coordinate.longitude;
+    }
+    return [[CLLocation alloc] initWithLatitude:lat/locations.count longitude:lon/locations.count];
+}
+@end
