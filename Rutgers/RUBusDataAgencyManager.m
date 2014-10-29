2421e0d065ee566d76a36ff10f626c33674c7581
kyle21b <oss@Opens-iMac.local>
2014-10-29 16:06:00 -0400

    changed retry timers

--- Rutgers/RUBusDataAgencyManager.m
+++ Rutgers/RUBusDataAgencyManager.m
@@ -155,7 +155,7 @@
         
     } failure:^(NSURLSessionDataTask *task, NSError *error) {
        
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(20 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(15 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [self getAgencyConfig];
             dispatch_group_leave(self.agencyGroup);
         });
@@ -175,7 +175,7 @@
 
     } failure:^(NSURLSessionDataTask *task, NSError *error) {
         
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(20 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(15 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [self getActiveStopsAndRoutes];
             dispatch_group_leave(self.activeGroup);
         });
