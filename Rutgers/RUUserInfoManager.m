0e47500ef559bb23f75d91c922770d34bb4891a3
kyle21b <oss@Opens-iMac.local>
2014-10-29 15:54:37 -0400

    removed time delay

--- Rutgers/RUUserInfoManager.m
+++ Rutgers/RUUserInfoManager.m
@@ -181,9 +181,7 @@ static NSString *const userInfoManagerUserRoleKey = @"userInfoManagerUserRoleKey
 }
 
 -(void)presentActionSheet:(UIActionSheet *)actionSheet{
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [actionSheet showInView:[[[UIApplication sharedApplication] delegate] window]];
-    });
+    [actionSheet showInView:[[[UIApplication sharedApplication] delegate] window].rootViewController.view];
 }
 
 
