9ba9077817f1800d4bcb47909eccfc1db9018b81
kyle21b <oss@woz.local>
2015-06-26 16:05:35 -0400

    placeholder separator inset changed

--- Rutgers/DataSource.m
+++ Rutgers/DataSource.m
@@ -157,6 +157,7 @@
 }
 
 -(void)configurePlaceholderCell:(id)cell{
+    [cell setSeparatorInset:UIEdgeInsetsZero];
     if ([cell isKindOfClass:[ALPlaceholderCell class]]) {
         ALPlaceholderCell *placeholder = cell;
         if ([self.loadingState isEqualToString:AAPLLoadStateNoContent]) {
