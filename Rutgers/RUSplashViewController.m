654db47baeb592ce295a866f8420ea4d80978a70
kyle21b <oss@woz.rutgers.edu>
2015-08-05 16:09:25 -0400

    splash updated

--- Rutgers/RUSplashViewController.m
+++ Rutgers/RUSplashViewController.m
@@ -9,7 +9,7 @@
 #import "RUSplashViewController.h"
 
 @interface RUSplashViewController ()
-
+@property (nonatomic) UIViewController *childVC;
 @end
 
 @implementation RUSplashViewController
@@ -23,11 +23,32 @@
     
     self.view.backgroundColor = [UIColor whiteColor];
     
+    UIViewController *viewController = [[UIViewController alloc] init];
     NSArray *nibContents = [[NSBundle mainBundle] loadNibNamed:@"Splash Screen" owner:nil options:nil];
-    UIView *splashView = [nibContents lastObject];
+    viewController.view = [nibContents lastObject];
     
-    [self.view addSubview:splashView];
-    [splashView autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
-    
+    [self.view addSubview:viewController.view];
+    [viewController.view autoPinEdgesToSuperviewEdgesWithInsets:UIEdgeInsetsZero];
+    [self addChildViewController:viewController];
+    [viewController didMoveToParentViewController:self];
+    self.childVC = viewController;
+    [self updateTraitCollectionForSize:self.view.bounds.size];
+
 }
+
+-(void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator{
+    [self updateTraitCollectionForSize:size];
+}
+
+-(void)updateTraitCollectionForSize:(CGSize)size{
+    [self setOverrideTraitCollection:[UITraitCollection traitCollectionWithHorizontalSizeClass:(size.width > size.height) ? UIUserInterfaceSizeClassRegular : UIUserInterfaceSizeClassCompact] forChildViewController:self.childVC];
+}
+
+-(BOOL)shouldAutomaticallyForwardAppearanceMethods{
+    return YES;
+}
+-(BOOL)shouldAutomaticallyForwardRotationMethods{
+    return YES;
+}
+
 @end
