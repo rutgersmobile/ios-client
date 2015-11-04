aa1a88c3d1b3108abf304a337deeb77ad7e793e7
kyle21b <oss@woz.rutgers.edu>
2015-11-04 14:29:19 -0500

    Added soc server test
    Tests every combination of campus semester and level

--- Rutgers Tests/RUSOCTest.m
+++ Rutgers Tests/RUSOCTest.m
@@ -16,6 +16,7 @@
 @end
 
 @implementation RUSOCTest
+
 -(void)testPreferences{
     RUSOCDataLoadingManager *manager = [RUSOCDataLoadingManager sharedInstance];
     XCTAssertNotNil(manager.campus);
@@ -31,11 +32,129 @@
     [manager performWhenLoaded:^(NSError *error) {
         [expectation fulfill];
     }];
-    [self waitForExpectationsWithTimeout:0.5 handler:nil];
+    [self waitForExpectationsWithTimeout:10.0 handler:nil];
     XCTAssertNotNil(manager.semester);
     XCTAssertNotNil(manager.semesters);
 }
 
+-(void)applyForEachCombination:(void(^)(NSDictionary *campus, NSDictionary *level, NSDictionary *semester))function{
+    RUSOCDataLoadingManager *manager = [RUSOCDataLoadingManager sharedInstance];
+
+    for (NSDictionary *campus in manager.campuses) {
+        for (NSDictionary *level in manager.levels) {
+            for (NSDictionary *semester in manager.semesters) {
+                
+                manager.semester = semester;
+                manager.campus = campus;
+                manager.level = level;
+                
+                function(campus,level,semester);
+                
+                usleep(1000*40);
+            }
+        }
+    }
+}
+
+-(void)testLoadingSubjectsForAllCombinations{
+    RUSOCDataLoadingManager *manager = [RUSOCDataLoadingManager sharedInstance];
+
+    XCTestExpectation *expectation = [self expectationWithDescription:@"semesterIndexLoad"];
+    [manager performWhenLoaded:^(NSError *error) {
+        XCTAssertNil(error, @"Error loading semester index file");
+        [expectation fulfill];
+    }];
+    [self waitForExpectationsWithTimeout:30 handler:nil];
+    
+    NSMutableDictionary *results = [NSMutableDictionary dictionary];
+    dispatch_queue_t queue = dispatch_queue_create("results mutation queue", DISPATCH_QUEUE_SERIAL);
+    
+    [self applyForEachCombination:^(NSDictionary *campus, NSDictionary *level, NSDictionary *semester) {
+        XCTestExpectation *expectation = [self expectationWithDescription:@"subjects"];
+        
+        NSString *title = [NSString stringWithFormat:@"%@ %@ %@",semester[@"title"],campus[@"title"],level[@"title"]];
+        
+        [manager getSubjectsWithCompletion:^(NSArray *subjects, NSError *error) {
+            dispatch_async(queue, ^{
+                NSMutableDictionary *result = [NSMutableDictionary dictionary];
+                if (error) result[@"error"] = error;
+                if (subjects) result[@"subjects"] = subjects;
+                results[title] = result;
+                [expectation fulfill];
+            });
+        }];
+        
+    }];
+    
+    [self waitForExpectationsWithTimeout:30 handler:nil];
+    
+    
+    [self applyForEachCombination:^(NSDictionary *campus, NSDictionary *level, NSDictionary *semester) {
+        NSString *title = [NSString stringWithFormat:@"%@ %@ %@",semester[@"title"],campus[@"title"],level[@"title"]];
+        
+        NSMutableDictionary *result = results[title];
+        
+        NSArray *subjects = result[@"subjects"];
+        
+        if (subjects.count > 0) {
+            NSDictionary *subject = subjects.firstObject;
+            NSString *code = subject[@"code"];
+            
+            XCTestExpectation *expectation = [self expectationWithDescription:@"subjects"];
+
+            [manager getCoursesForSubjectCode:code completion:^(NSArray *courses, NSError *error) {
+                dispatch_async(queue, ^{
+                    if (error) result[@"courseError"] = error;
+                    if (courses) result[@"courses"] = subjects;
+                    [expectation fulfill];
+                });
+            }];
+            
+            usleep(1000*30);
+        }
+        
+    }];
+    
+    [self waitForExpectationsWithTimeout:30 handler:nil];
+
+    [self applyForEachCombination:^(NSDictionary *campus, NSDictionary *level, NSDictionary *semester) {
+        NSString *title = [NSString stringWithFormat:@"%@ %@ %@",semester[@"title"],campus[@"title"],level[@"title"]];
+        
+        NSDictionary *result = results[title];
+        
+        NSArray *subjects = result[@"subjects"];
+        NSError *error = result[@"error"];
+        
+        if (subjects) {
+            if (subjects.count == 0) {
+                NSLog(@"*\tNo subjects for %@", title);
+            } else {
+                NSLog(@"*\tSuccessful loading subjects for %@", title);
+                
+                NSArray *courses = result[@"courses"];
+                NSError *courseError = result[@"courseError"];
+                
+                if (courses) {
+                    if (subjects.count == 0) {
+                        NSLog(@"\t\tNo courses for %@", title);
+                    } else {
+                        NSLog(@"\t\tSuccessful loading courses for %@", title);
+                    }
+                } else {
+                    NSLog(@"\t\tError Loading courses for %@", title);
+                    assert(!courseError && ![campus[@"tag"] isEqualToString:@"ONLINE"]);
+                    if (courseError) NSLog(@"\t\tError %@", courseError.localizedDescription);
+                }
+            }
+        } else {
+            NSLog(@"*\tError Loading subjects for %@", title);
+            if (error) NSLog(@"\t\tError %@", error.localizedDescription);
+        }
+    }];
+
+}
+
+/*
 -(void)testLoadingSubjects{
     RUSOCDataLoadingManager *manager = [RUSOCDataLoadingManager sharedInstance];
     XCTestExpectation *expectation = [self expectationWithDescription:@"subjects"];
@@ -45,7 +164,7 @@
         [expectation fulfill];
     }];
     [self waitForExpectationsWithTimeout:0.5 handler:nil];
-}
+}*/
 
 -(void)testLoadingSearchIndex{
     RUSOCSearchIndex *searchIndex = [[RUSOCSearchIndex alloc] init];
@@ -82,7 +201,7 @@
     [searchIndex performWhenLoaded:^(NSError *error) {
         [indexExpectation fulfill];
     }];
-    [self waitForExpectationsWithTimeout:0.5 handler:nil];
+    [self waitForExpectationsWithTimeout:10.0 handler:nil];
 }
 
 -(void)performSearchQuery:(NSString *)query onIndex:(RUSOCSearchIndex *)searchIndex{
@@ -92,7 +211,7 @@
         [searchExpectation fulfill];
     };
     [operation start];
-    [self waitForExpectationsWithTimeout:0.5 handler:nil];
+    [self waitForExpectationsWithTimeout:10.0 handler:nil];
 }
 
 
