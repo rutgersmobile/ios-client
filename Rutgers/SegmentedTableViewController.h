f945045ac61ae92b09b7db0c276f2c2b8890620b
kyle21b <oss@woz.rutgers.edu>
2015-12-07 17:26:17 -0500

    comments

--- Rutgers/SegmentedTableViewController.h
+++ Rutgers/SegmentedTableViewController.h
@@ -10,5 +10,11 @@
 
 @interface SegmentedTableViewController : TableViewController
 @property (nonatomic) UISegmentedControl *segmentedControl;
+
+/**
+ *  This is for a more sophisticated analytics system that details all the user interaction events
+ *  Here you would recieve the segment that was tapped on, and return details about it to send to the analytics server
+ *  @return A dictionary containing details about segment that was selected
+ */
 -(NSDictionary *)userInteractionForSegmentAtIndex:(NSInteger)segmentIndex;
 @end
