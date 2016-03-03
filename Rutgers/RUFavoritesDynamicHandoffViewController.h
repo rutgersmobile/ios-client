063cd1fada98d3ce56c965436f4dd4ca96aedaaf
kyle21b <oss@woz.rutgers.edu>
2016-03-02 20:58:12 -0500

    Handoff controller that will be needed when opening dtable/reader favorites

---
+++ Rutgers/RUFavoritesDynamicHandoffViewController.h
@@ -0,0 +1,13 @@
+//
+//  RUFavoritesDynamicHandoffViewController.h
+//  Rutgers
+//
+//  Created by Open Systems Solutions on 2/24/16.
+//  Copyright © 2016 Rutgers. All rights reserved.
+//
+
+#import "TableViewController.h"
+
+@interface RUFavoritesDynamicHandoffViewController : TableViewController
+
+@end
---
+++ Rutgers/RUFavoritesDynamicHandoffViewController.m
@@ -0,0 +1,37 @@
+//
+//  RUFavoritesDynamicHandoffViewController.m
+//  Rutgers
+//
+//  Created by Open Systems Solutions on 2/24/16.
+//  Copyright © 2016 Rutgers. All rights reserved.
+//
+
+#import "RUFavoritesDynamicHandoffViewController.h"
+
+@interface RUFavoritesDynamicHandoffViewController ()
+
+@end
+
+@implementation RUFavoritesDynamicHandoffViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
