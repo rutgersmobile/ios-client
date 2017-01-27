e75466bf05ece4d09eda450caef913298865651c
Matt Robinson <mattro@oit.rutgers.edu>
2017-01-27 16:25:43 -0500

    Added RxSwift to Moya

--- Rutgers/RutgersAPI.swift
+++ Rutgers/RutgersAPI.swift
@@ -9,29 +9,18 @@
 import Foundation
 import Moya
 import Unbox
+import RxSwift
 
 class RutgersAPI {
     static let sharedInstance = RutgersAPI()
-    
-    let provider : MoyaProvider<RutgersService>
+
+    let provider : RxMoyaProvider<RutgersService>
     
     private init() {
-        self.provider = MoyaProvider<RutgersService>()
+        self.provider = RxMoyaProvider<RutgersService>()
     }
 
-    public func getDiningHalls() {
-        provider.request(.getDiningHalls) { result in
-            switch result {
-            case let .success(response):
-                do {
-                    let diningHalls: [DiningHall] = try unbox(data: response.data)
-                    print(diningHalls)
-                } catch {
-                    print("Error parsing \(error)")
-                }
-            case let .failure(error):
-                print("Error requesting \(error)")
-            }
-        }
+    public func getDiningHalls() -> Observable<[DiningHall]> {
+        return self.provider.request(.getDiningHalls).mapUnboxArray(type: DiningHall.self)
     }
 }
