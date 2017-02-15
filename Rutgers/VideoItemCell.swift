b55a9373dd84e0f1315499c657bfb860a99cb49a
Colin Walsh <colinfwalsh@gmail.com>
2017-02-15 15:58:27 -0500

    Updating CinemaCollectionViewController

---
+++ Rutgers/VideoItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  VideoItemCell.swift
+//  Rutgers
+//
+//  Created by cfw37 on 2/15/17.
+//  Copyright © 2017 Rutgers. All rights reserved.
+//
+
+import UIKit
+
+class VideoItemCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
