f8ca7bdd64a9008822d9e4d51ce62895e0759238
Matt Robinson <mattro@oit.rutgers.edu>
2017-02-15 13:39:55 -0500

    Storyboard stuff

--- Rutgers/RUFoodCollectionViewCell.swift
+++ Rutgers/RUFoodCollectionViewCell.swift
@@ -8,6 +8,6 @@
 
 import Foundation
 
-class RUFoodCollectionViewControllerCell: UICollectionViewCell {
+class RUFoodCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var label: UILabel!
 }
