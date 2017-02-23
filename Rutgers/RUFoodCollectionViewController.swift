b8a55810c563b9fc0d3e11918afb2eb2cad56f92
Matt Robinson <mattro@oit.rutgers.edu>
2017-02-23 16:30:22 -0500

    Basic RxDataSources food stuff

--- Rutgers/RUFoodCollectionViewController.swift
+++ Rutgers/RUFoodCollectionViewController.swift
@@ -7,10 +7,20 @@
 //
 
 import Foundation
+import RxSwift
+import RxDataSources
 
-class RUFoodCollectionViewController: UICollectionViewController, RUChannelProtocol {
+class RUFoodCollectionViewController
+    : UICollectionViewController
+    , RUChannelProtocol
+{
     var channel: [NSObject : AnyObject] = [:]
     let cellId = "FoodCellId"
+
+    let disposeBag = DisposeBag()
+
+    typealias DiningHallDataSource =
+        RxCollectionViewSectionedReloadDataSource<DiningHallSection>
 
     static func channelHandle() -> String! {
         return "food";
@@ -25,7 +35,8 @@ class RUFoodCollectionViewController: UICollectionViewController, RUChannelProto
         withConfiguration channelConfiguration: [AnyHashable : Any]!
     ) -> Any! {
         let storyboard = UIStoryboard(name: "RUFoodStoryboard", bundle: nil)
-        let me = storyboard.instantiateInitialViewController() as! RUFoodCollectionViewController
+        let me = storyboard.instantiateInitialViewController()
+            as! RUFoodCollectionViewController
         me.channel = channelConfiguration as [NSObject : AnyObject]
         return me
     }
@@ -36,5 +47,76 @@ class RUFoodCollectionViewController: UICollectionViewController, RUChannelProto
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView?.dataSource = nil
+
+        let dataSource = DiningHallDataSource()
+
+        dataSource.configureCell = {(
+            ds: CollectionViewSectionedDataSource<DiningHallSection>,
+            cv: UICollectionView,
+            ip: IndexPath,
+            item: DiningHallSectionItem
+        ) in
+            let cell = cv.dequeueReusableCell(
+                withReuseIdentifier: self.cellId,
+                for: ip
+            ) as! RUFoodCollectionViewCell
+
+            cell.label.text = {
+                switch item {
+                case .fullMenu(let diningHall):
+                    return diningHall.name
+                case .stubMenu(let stub):
+                    return stub
+                }
+            }()
+
+            return cell
+        }
+
+        let newarkSection = DiningHallSection(
+            header: "Newark",
+            items: [.stubMenu("Newark Menu")]
+        )
+
+        let camdenSection = DiningHallSection(
+            header: "Camden",
+            items: [.stubMenu("Camden")]
+        )
+
+        RutgersAPI.sharedInstance.getDiningHalls()
+            .map { halls in DiningHallSection(
+                header: "New Brunswick",
+                items: halls.map { .fullMenu($0) }
+            )}
+            .toArray()
+            .map { sections in sections + [newarkSection, camdenSection] }
+            .asDriver(onErrorJustReturn: [])
+            .drive(self.collectionView!.rx.items(dataSource: dataSource))
+            .addDisposableTo(disposeBag)
+    }
+}
+
+struct DiningHallSection {
+    var header: String
+    var items: [Item]
+
+    init(header: String, items: [Item]) {
+        self.header = header
+        self.items = items
+    }
+}
+
+enum DiningHallSectionItem {
+    case fullMenu(DiningHall)
+    case stubMenu(String)
+}
+
+extension DiningHallSection: SectionModelType {
+    typealias Item = DiningHallSectionItem
+
+    init(original: DiningHallSection, items: [Item]) {
+        self = original
+        self.items = items
     }
 }
