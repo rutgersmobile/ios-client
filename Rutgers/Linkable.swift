5a709cb119330d7d85fd3bc5e2855cde7a990640
Matt Robinson <mattro@oit.rutgers.edu>
2017-02-27 15:49:25 -0500

    Linkable protocol added

---
+++ Rutgers/Linkable.swift
@@ -0,0 +1,13 @@
+//
+//  Linkable.swift
+//  Rutgers
+//
+//  Created by Matt Robinson on 2/27/17.
+//  Copyright © 2017 Rutgers. All rights reserved.
+//
+
+import Foundation
+
+protocol Linkable {
+    func sharingUrl() -> URL?
+}
