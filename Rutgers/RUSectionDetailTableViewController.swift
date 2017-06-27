e4d2034f117912fb7849a9d1faaaa44f163781d6
Colin Walsh <cfw37@Opens-iMac.local>
2017-06-27 16:29:25 -0400

    Cells displaying properly, need to format text, fix cell layouts

--- Rutgers/RUSectionDetailTableViewController.swift
+++ Rutgers/RUSectionDetailTableViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 import RxSwift
 import RxDataSources
 
+class RUSOCDetailCell: UITableViewCell {
+    
+}
+
+class RUSOCLocationCell: UITableViewCell {
+    
+}
+
+class RUSOCSectionDetailCell: UITableViewCell {
+    
+}
+
 class RUSOCSectionDetailTableViewController: UITableViewController {
     
     var section: Section!
+    let disposeBag = DisposeBag()
     
     static func instantiate(
         withStoryboard storyboard: UIStoryboard,
@@ -32,69 +45,149 @@ class RUSOCSectionDetailTableViewController: UITableViewController {
         
         self.tableView.dataSource = nil
         
-        let dataSource = RxTableViewSectionedReloadDataSource<SectionDetailSection>()
+        
+        let dataSource = RxTableViewSectionedReloadDataSource<MultiSection>()
         
         dataSource.configureCell = { (
-            ds: TableViewSectionedDataSource<SectionDetailSection>,
+            ds: TableViewSectionedDataSource<MultiSection>,
             tv: UITableView,
-            ip: IndexPath,
-            item: SectionDetailItem
+            idxPath: IndexPath,
+            item: SOCSectionDetailItem
             ) in
-            switch item {
-            case .section(let section):
+            switch dataSource[idxPath] {
+            case let .noteSectionItem(section):
+                let cell = tv.dequeueReusableCell(
+                    withIdentifier: "notesCell",
+                    for: idxPath
+                )
+                
+                print(section)
+                cell.textLabel?.text = "Section notes go here"
+                
+                cell.setupCellLayout()
+                return cell
+            case let .sectionItem(section):
                 let cell = tv.dequeueReusableCell(
                     withIdentifier: "sectionCell",
-                    for: ip
+                    for: idxPath
                     )
                 
+                cell.textLabel!.text = "Section \(section.number)"
+                
+                
                 cell.setupCellLayout()
                 return cell
-            case .detail(let section):
+            case let .defaultItem(item):
                 let cell = tv.dequeueReusableCell(
                     withIdentifier: "defaultCell",
-                    for: ip
+                    for: idxPath
                 )
                 
-                cell.detailTextLabel?.text = section.instructors[0].instructorName 
-                cell.textLabel?.text = "Intructors"
-                
+                switch item{
+                case is MeetingTime:
+                    let item = item as! MeetingTime
+                    
+                    if let day = item.meetingDay {
+                        cell.textLabel?.text = day
+                    }
+                    
+                    if let startTime = item.startTime {
+                        cell.detailTextLabel?.text = "\(startTime)-\(item.endTime!)"
+                    } else {
+                        cell.detailTextLabel?.text = ""
+                    }
+                    
+                default:
+                    cell.textLabel?.text = "\(item)"
+                }
                 cell.setupCellLayout()
                 return cell
-            case .location(let section):
+            case let .locationItem(section):
                 let cell = tv.dequeueReusableCell(
                     withIdentifier: "locationCell",
-                    for: ip
+                    for: idxPath
                 )
+                
+                cell.textLabel?.text = "\(section.sectionIndex)"
+                
                 cell.setupCellLayout()
                 return cell
             }
+            
         }
         
+        dataSource.titleForHeaderInSection = { (ds, idxPath) in
+            ds.sectionModels[idxPath].title
+        }
         
+        let toDrive: [MultiSection] =
+            [
+            .HeaderSection(
+                items: [.noteSectionItem(section: self.section),
+                        .sectionItem(section: self.section)]),
+            .MeetingTimesSection(title: "Meeting Times",
+                                 items:
+                                    self.section
+                                        .meetingTimes
+                                        .map{.defaultItem(item:$0)}),
+            .LocationSection(title: "Locations",
+                              items: [.locationItem(section: self.section)])
+            
+            ]
+        
+        Observable.just(toDrive)
+            .asDriver(onErrorJustReturn: [])
+            .drive(self.tableView!.rx.items(dataSource: dataSource))
+            .addDisposableTo(self.disposeBag)
+        }
     }
+
+
+private enum MultiSection {
+    case HeaderSection(items: [SOCSectionDetailItem])
+    case MeetingTimesSection(title: String, items: [SOCSectionDetailItem])
+    case LocationSection(title: String, items: [SOCSectionDetailItem])
 }
 
-struct SectionDetailSection {
-    var title: String
-    var items: [Item]
+private enum SOCSectionDetailItem {
+    case noteSectionItem(section: Section)
+    case sectionItem(section: Section)
+    case defaultItem(item: Any)
+    case locationItem(section: Section)
+}
+
+extension MultiSection: SectionModelType {
     
-    init(title: String, items: [Item]) {
-        self.title = title
-        self.items = items
+    var items: [SOCSectionDetailItem] {
+        switch self {
+        case .HeaderSection(items: let items):
+            return items
+        case .MeetingTimesSection(title: _, items: let items):
+            return items
+        case .LocationSection(title: _, items: let items):
+            return items
+        }
     }
-}
-
-enum SectionDetailItem {
-    case section(Section)
-    case detail(Section)
-    case location(Section)
-}
-
-extension SectionDetailSection: SectionModelType {
-    typealias Item = SectionDetailItem
     
-    init(original: SectionDetailSection, items: [Item]) {
-        self = original
-        self.items = items
+    var title: String {
+        switch self {
+        case .HeaderSection(items: _):
+            return ""
+        case .MeetingTimesSection(title: let title, items: _):
+            return title
+        case .LocationSection(title: let title, items: _):
+            return title
+        }
+    }
+    
+    init(original: MultiSection, items: [SOCSectionDetailItem]) {
+        switch original {
+        case let .HeaderSection(items: items):
+            self = .HeaderSection(items: items)
+        case let .MeetingTimesSection(title: title, items: _):
+            self = .MeetingTimesSection(title: title, items: items)
+        case let .LocationSection(title: title, items: _):
+            self = .LocationSection(title: title, items: items)
+        }
     }
 }
